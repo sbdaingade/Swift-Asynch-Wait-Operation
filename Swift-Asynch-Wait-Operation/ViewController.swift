e03d4fec2d743b3574b3176344f1e977f9090582
Sachin Daingade <sbdaingade@gmail.com>
2021-12-16 06:27:38 +0530

    project added

---
+++ Swift-Asynch-Wait-Operation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-Asynch-Wait-Operation
+//
+//  Created by Sachin Daingade on 16/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift-Asynch-Wait-OperationTests/Swift_Asynch_Wait_OperationTests.swift
@@ -0,0 +1,33 @@
+//
+//  Swift_Asynch_Wait_OperationTests.swift
+//  Swift-Asynch-Wait-OperationTests
+//
+//  Created by Sachin Daingade on 16/12/21.
+//
+
+import XCTest
+@testable import Swift_Asynch_Wait_Operation
+
+class Swift_Asynch_Wait_OperationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
